8b41d32434b0d4fd397575d0321794715f3fe073
Tech Funda <techfunda@proton.me>
2024-10-07 09:45:53 -0700

    initial

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module use-mod-from-workspace
 
 go 1.23.2
 
-require github.com/techfunda4all/workspace v0.0.0-20241007155304-102d955e31ef
+require (
+	github.com/techfunda4all/workspace/module-1 v0.0.0-20241007163530-14f37af4def4 // indirect
+	github.com/techfunda4all/workspace/module-2 v0.0.0-20241007163530-14f37af4def4 // indirect
+)
--- use-mod-from-workspace.go
+++ use-mod-from-workspace.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
 import (
-	mod1 "github.com/techfunda4all/workspace/module-1"
-	mod2 "github.com/techfunda4all/workspace/module-2"
+	"fmt"
+	module1 "github.com/techfunda4all/workspace/module-1"
+	module2 "github.com/techfunda4all/workspace/module-2"
 )
 
 func main() {
-	fmt.Println(mod1.Name())
-	fmt.Println(mod2.Name())
+	fmt.Println(module1.Name())
+	fmt.Println(module2.Name())
 }
